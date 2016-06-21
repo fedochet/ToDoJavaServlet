c34e588183d490e2b4b16c29528772c734fa05be
fedochet <fedochet@yandex.ru>
2016-06-21 19:00:02 +0300

    simple todo html page

--- src/main/java/org/anstreth/controllers/MyServlet.java
+++ src/main/java/org/anstreth/controllers/MyServlet.java
@@ -1,28 +1,58 @@
 package org.anstreth.controllers;
 
+import org.anstreth.todo.ToDoList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Created by roman on 14.06.2016.
  */
 
-@WebServlet(urlPatterns = {"/"})
+@WebServlet("/")
 public class MyServlet extends HttpServlet {
+    public static final String CompletedItemsKey = "completed_items";
+    public static final String UncompletedItemsKey = "uncompleted_items";
+
+    private ToDoList list = new ToDoList();
+
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        req.setAttribute("new_task", newTask);
+        req.setAttribute(CompletedItemsKey, list.getCompletedItems());
+        req.setAttribute(UncompletedItemsKey, list.getUncompletedItems());
         req.getRequestDispatcher("index.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        newTask = req.getParameter("new_task");
-        resp.sendRedirect("");
+        if (req.getServletPath() != null) {
+            switch (req.getServletPath()) {
+                case "/complete":
+                    String taskToComplete = req.getParameter("task_name");
+                    if (taskToComplete!=null && !taskToComplete.isEmpty() && !list.isCompleted(taskToComplete)) {
+                        list.complete(taskToComplete);
+                    }
+                    resp.sendRedirect("");
+                    break;
+                case "/uncomplete":
+                    String taskToUncomplete = req.getParameter("task_name");
+                    if (taskToUncomplete!=null && !taskToUncomplete.isEmpty() && list.isCompleted(taskToUncomplete)) {
+                        list.uncomplete(taskToUncomplete);
+                    }
+                    resp.sendRedirect("");
+                    break;
+                case "/add":
+                    String newTask = req.getParameter("new_task");
+                    if (newTask != null && !newTask.isEmpty()) {
+                        list.addItem(newTask);
+                    }
+                    resp.sendRedirect("");
+                    break;
+            }
+        }
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -6,7 +6,7 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib  uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,11 +15,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-<form method="post" action="">
+<form method="post" action="add">
     New ToDo: <br>
     <input type="text" name="new_task"><br>
     <input type="submit" value="Add">
 </form>
-<c:out value="${new_task}"/>
+<B>Uncompleted items</B><br/>
+<c:forEach items="${uncompleted_items}" var="item">
+    <form action="complete" method="post">
+        <input type="checkbox" onclick="this.form.submit();" name="task_name" value="<c:out value="${item}"/>"><c:out value="${item}"/><br/>
+    </form>
+</c:forEach>
+<B>Completed items</B><br/>
+<c:forEach items="${completed_items}" var="item">
+    <form action="uncomplete" method="post">
+        <input type='hidden' name="task_name" value="<c:out value="${item}"/>">
+        <input type="checkbox" onclick="this.form.submit();" checked="true"><c:out value="${item}"/><br/>
+    </form>
+</c:forEach>
+<%--<input name="your_name" value="your_value" type="checkbox">Checkbox text<br/>--%>
 </body>
 </html>
